f9bfe9845254ab4a170b47cc6a6c51c9fcc09513
GavonC <99353848+GavonC@users.noreply.github.com>
2023-11-10 09:36:15 -0600

    fuck

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tool', toolRouter);
+app.use('./index', indexRouter);
+app.use('./users', usersRouter);
+app.use('./tool', toolRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
